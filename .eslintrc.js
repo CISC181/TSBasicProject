72cd4c5519d7b165a52bf4c424d94a9bc737602d
Austin Cory Bart <acbart@vt.edu>
2024-02-02 11:17:09 -0500

    Use the new rules, but override just that one

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
     env: {
-        node: true
+        es2022: true,
+        node: true,
     },
+    extends: [
+        "eslint:recommended",
+    ],
     root: true,
     parser: '@typescript-eslint/parser',
     plugins: [
-        '@typescript-eslint',
+        '@typescript-eslint'
     ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+    "overrides": [
+        {
+            "files": ["test/**/*"],
+            "env": {
+                "jest": true
+            }
+        }
     ],
     rules: {
-        'semi': ['error', 'always'],
-        'no-console': 2,
-        'quotes': ['error', 'single'],
-        'curly': 0,
-        'brace-style': ['error', 'stroustrup'],
-        'indent': ['error', 4],
-        'eol-last': ['error', 'never'],
-        '@typescript-eslint/no-unused-vars': 2
+        'eol-last': ['error', 'never']
     }
 };
